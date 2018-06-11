63c26d88421de94252c9d51058b35f6ceb62d158
Jordan Harris <jharris@wework.com>
2018-06-11 16:29:52 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,16 +1,10 @@
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
 
-    expect(age_of_person).to eq(19)
+def current_age_for_birth_year(birth_year)
+  current_year = Time.now.year
+  answer = current_year - birth_year
   end
 end
 
-
-def current_age_for_birth_year(birth_year, x)
-  current_year = Time.now.year
-  birth_year = x
-  answer = current_year - birth_year
-
-
-
+current_age_for_birth_year(1984)
